08b74dc4b8a4611ec784daed4c1dd68ffab69e26
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-07-12 14:33:07 +0800

    Remove if accSetting btn is double click it closes feature

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ let navLinks = document.querySelector('.nav-links');
 let accountSetting = document.querySelector('.account-setting');
 let accountBtn = document.querySelectorAll('.accBtn');
 let screenDarken = document.querySelector('.darken-screen');
-let accSettingClick = 0;
 
 headerBtn.forEach((item)=>{
     item.addEventListener('click', ()=>{
@@ -15,23 +14,15 @@ headerBtn.forEach((item)=>{
 accountBtn.forEach((item)=>{
     item.addEventListener('click', ()=>{
         accountSetting.style.display = 'flex';
-        accSettingClick++;
     });
 });
 
 window.addEventListener('click', (e)=>{
     console.log(e.target);
-    console.log(accSettingClick);
     if(e.target.getAttribute('data-click') != 'doNothing'){
         navLinks.style.left = "-400px"
         screenDarken.style.display = 'none';
         accountSetting.style.display = 'none';
     }
-
-    if(accSettingClick == 4){
-        accountSetting.style.display = 'none';
-        accSettingClick = 0;
-    }
-
 });
 
