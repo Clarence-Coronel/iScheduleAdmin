cc6a170070ea777eca89ffaea3ff799b81c46e48
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-07-14 17:50:34 +0800

    Populate nav-links

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ let navLinks = document.querySelector('.nav-links');
 let accountSetting = document.querySelector('.account-setting');
 let accountBtn = document.querySelectorAll('.accBtn');
 let screenDarken = document.querySelector('.darken-screen');
+let navLinkItem = document.querySelectorAll('.nav-links__item');
 let desktopMode = false;
 
 headerBtn.forEach((item)=>{
     item.addEventListener('click', ()=>{
         navLinks.style.left = "0";
         screenDarken.style.display = 'block';
-        
     });
 });
 
@@ -22,11 +22,21 @@ accountBtn.forEach((item)=>{
 window.addEventListener('click', (e)=>{
     console.log(e.target);
     if(e.target.getAttribute('data-click') != 'doNothing'){
-        navLinks.style.left = "-300px"
+        navLinks.style.left = "-250px"
         screenDarken.style.display = 'none';
         
         accountSetting.style.display = 'none';
     }
+});
+
+navLinkItem.forEach((item)=>{
+    item.addEventListener('mouseover',()=>{
+        item.querySelector('.ico-nav').style.color = 'white';
+    });
+
+    item.addEventListener('mouseout', ()=>{
+        item.querySelector('.ico-nav').style.color = 'rgb(210, 208, 208)';
+    });
 });
 
 
