fcee6039b89612e7ea531251b58d68fb0bd1f041
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-07-15 23:21:01 +0800

    Add 'Create New Admin' page.
    Aside from creating create new admin also implemented AOS.
    Added a container so AOS and my animation won't conflict.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,30 @@ let accountSetting = document.querySelector('.account-setting');
 let accountBtn = document.querySelectorAll('.accBtn');
 let screenDarken = document.querySelector('.darken-screen');
 let navLinkItem = document.querySelectorAll('.nav-links__item');
+let inputContainer = document.querySelectorAll('.input-container');
 let desktopMode = false;
+
+
+// AOS Initialization
+AOS.init();
+
+inputContainer.forEach((item)=>{
+
+    try {
+        item.querySelector('input').addEventListener('focusin',()=>{
+            item.style.borderColor = '#4e73df'; //primary-color
+        })
+    
+        item.querySelector('input').addEventListener('focusout',()=>{
+            if(item.querySelector('input').value == ""){
+                item.style.borderColor = 'rgb(80, 78, 78)'; //fontbase-color
+            }
+        })
+    } catch (error) {
+        
+    }
+        
+});
 
 headerBtn.forEach((item)=>{
     item.addEventListener('click', ()=>{
@@ -20,7 +43,7 @@ accountBtn.forEach((item)=>{
 });
 
 window.addEventListener('click', (e)=>{
-    console.log(e.target);
+    // console.log(e.target);
     if(e.target.getAttribute('data-click') != 'doNothing'){
         navLinks.style.left = "-250px"
         screenDarken.style.display = 'none';
