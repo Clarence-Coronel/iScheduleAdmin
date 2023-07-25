4a10f2f4c1a71e1dca903bc591294b84d350810a
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-07-25 17:31:05 +0800

    Add manageData to html content and clean the code.

--- js/htmlContent.js
+++ js/htmlContent.js
@@ -1,67 +1,5 @@
 const main = document.querySelector('main');
 
-let createAccount = `
-<section class="add-admin">
-<div class="form-container"  data-aos="fade-right" data-aos-duration="1000">
-    <form class="form" autocomplete="off" onsubmit="return createAccountValidator()">
-        <div class="custom-shape-divider-top-1689432371">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
-                <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
-                <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
-            </svg>
-        </div>
-        <h2>
-            <span class="material-icons-outlined ico-nav">person_add_alt</span>
-            Admin Information
-        </h2>
-        <div class="input-container">
-            <input type="text" name="firstName" id="firstName" required>
-            <label for="firstName">First Name</label>
-        </div>
-        <div class="input-container">
-            <input type="text" name="middleName" id="middleName" required>
-            <label for="middleName">Middle Name</label>
-        </div>
-        <div class="input-container">
-            <input type="text" name="lastName" id="lastName" required>
-            <label for="lastName">Last Name</label>
-        </div>
-        <div class="input-container">
-            <input type="text" name="phone" id="phone" required oninput="filterPhoneInput(this.id)">
-            <label for="phone">Phone #</label>
-        </div>
-        <div class="input-container">
-            <input type="password" name="password" id="password" required onpaste="return false;" ondrop="return false;">
-            <span class="material-icons-outlined ico-pass">visibility_off</span>
-            <label for="password">Password</label>
-        </div>
-        <div class="input-container">
-            <input type="password" name="confirm-password" id="confirmPassword" required onpaste="return false;" ondrop="return false;">
-            <span class="material-icons-outlined ico-pass">visibility_off</span>
-            <label for="confirmPassword">Confirm Password</label>
-        </div>
-        <div class="radio-container">
-            <div class="radiobtn-container">
-                <input type="radio" id="adminI" name="adminType" value="adminI" required onclick="generateAdminTypeModal(0)">
-                <label for="adminI">Admin I</label>
-            </div>
-            <div class="radiobtn-container">
-                <input type="radio" id="adminII" name="adminType" value="adminII" required onclick="generateAdminTypeModal(1)">
-                <label for="adminII">Admin II</label>
-            </div>
-            <div class="radiobtn-container">
-                <input type="radio" id="superAdmin" name="adminType" value="superAdmin" required onclick="generateAdminTypeModal(2)">
-                <label for="superAdmin">Super Admin</label>
-            </div>
-        </div>
-        <button class="btn--addAdmin" formnovalidate>Create</button>
-        </div>
-    </form>
-</div>
-</section>`;
-
-
 // By default yung content ni table is yung today lang
 let adminLogs = `<section class="admin-logs">
 <div class="admin-logs-wrapper" data-aos="fade-right" data-aos-duration="1000">
@@ -320,6 +258,125 @@ let adminList = `<section class="admin-list">
 </div>
 </section>`;
 
+let createAccount = `<section class="add-admin">
+<div class="form-container"  data-aos="fade-right" data-aos-duration="1000">
+    <form class="form" autocomplete="off" onsubmit="return createAccountValidator()">
+        <div class="custom-shape-divider-top-1689432371">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+                <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+                <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+            </svg>
+        </div>
+        <h2>
+            <span class="material-icons-outlined ico-nav">person_add_alt</span>
+            Admin Information
+        </h2>
+        <div class="input-container">
+            <input type="text" name="firstName" id="firstName" required>
+            <label for="firstName">First Name</label>
+        </div>
+        <div class="input-container">
+            <input type="text" name="middleName" id="middleName" required>
+            <label for="middleName">Middle Name</label>
+        </div>
+        <div class="input-container">
+            <input type="text" name="lastName" id="lastName" required>
+            <label for="lastName">Last Name</label>
+        </div>
+        <div class="input-container">
+            <input type="text" name="phone" id="phone" required oninput="filterPhoneInput(this.id)">
+            <label for="phone">Phone #</label>
+        </div>
+        <div class="input-container">
+            <input type="password" name="password" id="password" required onpaste="return false;" ondrop="return false;">
+            <span class="material-icons-outlined ico-pass">visibility_off</span>
+            <label for="password">Password</label>
+        </div>
+        <div class="input-container">
+            <input type="password" name="confirm-password" id="confirmPassword" required onpaste="return false;" ondrop="return false;">
+            <span class="material-icons-outlined ico-pass">visibility_off</span>
+            <label for="confirmPassword">Confirm Password</label>
+        </div>
+        <div class="radio-container">
+            <div class="radiobtn-container">
+                <input type="radio" id="adminI" name="adminType" value="adminI" required onclick="generateAdminTypeModal(0)">
+                <label for="adminI">Admin I</label>
+            </div>
+            <div class="radiobtn-container">
+                <input type="radio" id="adminII" name="adminType" value="adminII" required onclick="generateAdminTypeModal(1)">
+                <label for="adminII">Admin II</label>
+            </div>
+            <div class="radiobtn-container">
+                <input type="radio" id="superAdmin" name="adminType" value="superAdmin" required onclick="generateAdminTypeModal(2)">
+                <label for="superAdmin">Super Admin</label>
+            </div>
+        </div>
+        <button class="btn--addAdmin" formnovalidate>Create</button>
+        </div>
+    </form>
+</div>
+</section>`;
+
+let websiteStatus = `<section class="website-status">
+<div class="website-status__wrapper" data-aos="fade-right" data-aos-duration="1000">
+    <div class="status">
+        <div class="status__header">
+            <!-- highlight_off = kapag down website -->
+            <!-- block =kapag down scheduling -->
+            <span class="material-icons-outlined ico-status ico-live">check_circle_outline</span>
+            Website is Up
+            <!-- Possible Values
+                *Website Is Up
+                *Website Is Down
+                *Scheduling Is Down
+            -->
+        </div>
+        <div class="status__msg">
+            <span class="msg-label">Message:</span> loresmloresmloresmloresmloresmloresmloresmloresmloresmloresmloresmloresmloresm loresmloresmloresmloresmloresm loresmloresmloresmloresmloresmloresm loresmloresmloresmloresmloresm
+            <!-- Applicable lang if either Website is Down or Online Scheduling is down -->
+        </div>
+    </div>
+    <div class="change-status">
+        <!-- <h3>Change Status</h3> -->
+        <select class="form-select" aria-label="Default select example">
+            <option value="up">Website is Up</option>
+            <option value="down">Website is Down</option>
+            <option value="scheduledown">Scheduling is Down</option>
+        </select>
+        <!-- Naka enable lang to if either website is down or scheduling is down hindi kapag completely up si website -->
+        <div class="msg-container">
+            <label for="msg">Message:</label>
+            <textarea name="text" id="statusMsg" cols="30" rows="2" onblur="inputLimiterBlur(this.id, 120); statusMsgCounter();" oninput="inputLimiter(this.id, 120); statusMsgCounter();"></textarea>
+            <div class="textAreaCounter">120</div>
+        </div>
+        <button class="changeStatus" onclick="alert('give warning regarding new status, such as what will happen')">Apply</button>
+    </div>
+</div>
+</section>`;
+
+// Add modal when delete is pressed confirming the action and then another modal to obtain OTP  from super admin's number  for additional security
+let manageData = `<section class="manage-data">
+<div class="manage-date__content">
+    <div class="field">
+        <span class="field__name">Admin Logs</span>
+        <p class="field__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quidem mollitia numquam pariatur autem.</p>
+        <button class="field__delete-btn">Delete</button>
+    </div>
+    <div class="field">
+        <span class="field__name">Archived Appointments</span>
+        <p class="field__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quidem mollitia numquam pariatur autem.</p>
+        <button class="field__delete-btn">Delete</button>
+    </div>
+    <div class="field">
+        <span class="field__name">Active Appointments</span>
+        <p class="field__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quidem mollitia numquam pariatur autem.</p>
+        <button class="field__delete-btn">Delete</button>
+    </div>
+</div>
+</section>`;
+
+
 // PAG PININDOT YUNG REPEAT EVERY YEAR MADISABLE YUNG YEAR FIELD
 let blockDates = `
 <section class="block-date">
@@ -465,42 +522,24 @@ let blockDates = `
             </div>
 </section>`
 
-let websiteStatus = `<section class="website-status">
-<div class="website-status__wrapper" data-aos="fade-right" data-aos-duration="1000">
-    <div class="status">
-        <div class="status__header">
-            <!-- highlight_off = kapag down website -->
-            <!-- block =kapag down scheduling -->
-            <span class="material-icons-outlined ico-status ico-live">check_circle_outline</span>
-            Website is Up
-            <!-- Possible Values
-                *Website Is Up
-                *Website Is Down
-                *Scheduling Is Down
-            -->
-        </div>
-        <div class="status__msg">
-            <span class="msg-label">Message:</span> loresmloresmloresmloresmloresmloresmloresmloresmloresmloresmloresmloresmloresm loresmloresmloresmloresmloresm loresmloresmloresmloresmloresmloresm loresmloresmloresmloresmloresm
-            <!-- Applicable lang if either Website is Down or Online Scheduling is down -->
-        </div>
-    </div>
-    <div class="change-status">
-        <!-- <h3>Change Status</h3> -->
-        <select class="form-select" aria-label="Default select example">
-            <option value="up">Website is Up</option>
-            <option value="down">Website is Down</option>
-            <option value="scheduledown">Scheduling is Down</option>
-        </select>
-        <!-- Naka enable lang to if either website is down or scheduling is down hindi kapag completely up si website -->
-        <div class="msg-container">
-            <label for="msg">Message:</label>
-            <textarea name="text" id="statusMsg" cols="30" rows="2" onblur="inputLimiterBlur(this.id, 120); statusMsgCounter();" oninput="inputLimiter(this.id, 120); statusMsgCounter();"></textarea>
-            <div class="textAreaCounter">120</div>
-        </div>
-        <button class="changeStatus" onclick="alert('give warning regarding new status, such as what will happen')">Apply</button>
-    </div>
-</div>
-</section>`;
+
+// PAGE GENERATORS
+
+function generateDashboard(){
+    main.innerHTML = "";
+}
+
+function generateAdminLogs(){
+    contentIsOpen = true;
+    main.innerHTML = adminLogs;
+    showTableCell();
+}
+
+function generateAdminList(){
+    contentIsOpen = true;
+    main.innerHTML = adminList;
+    showTableCell();
+}
 
 function generateCreateAcc(){
     main.innerHTML = createAccount;
@@ -527,20 +566,14 @@ function generateCreateAcc(){
             }
         })
     });
-
-
 }
 
-function generateAdminLogs(){
-    contentIsOpen = true;
-    main.innerHTML = adminLogs;
-    showTableCell();
+function generateWebsiteStatus(){
+    main.innerHTML = websiteStatus;
 }
 
-function generateAdminList(){
-    contentIsOpen = true;
-    main.innerHTML = adminList;
-    showTableCell();
+function generateManageData(){
+    main.innerHTML = manageData;
 }
 
 function generateBlockDates(){
@@ -548,10 +581,5 @@ function generateBlockDates(){
     showTableCell();
 }
 
-function generateDashboard(){
-    main.innerHTML = "";
-}
 
-function generateWebsiteStatus(){
-    main.innerHTML = websiteStatus;
-}
+
