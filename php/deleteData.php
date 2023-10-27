adf4e866cb3de500621197f58cb40929dc8374a0
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-10-27 16:54:20 +0800

    Add reset of auto increment

--- php/deleteData.php
+++ php/deleteData.php
@@ -5,6 +5,7 @@
 
     $code = null;
     $query = null;
+    $subQuery = null;
     $subj = null;
 
     foreach($_POST as $temp){
@@ -13,26 +14,35 @@
 
     if($code == 0){
         $query = "DELETE FROM `admin_logs` WHERE 1;";
+        $subQuery = "ALTER TABLE admin_logs AUTO_INCREMENT = 1";
         $subj = "admin logs";
     }
     else if($code == 1){
         $query = "DELETE FROM `appointments` WHERE 1;";
+        $subQuery = "ALTER TABLE appointments AUTO_INCREMENT = 1";
         $subj = "appointments";
     }
     else if($code == 2){
         $query = "DELETE FROM `feedbacks` WHERE 1;";
+        $subQuery = "ALTER TABLE feedbacks AUTO_INCREMENT = 1";
         $subj = "feedbacks";
     }
     else if($code == 3){
         $query = "DELETE FROM `announcements` WHERE 1;";
+        $subQuery = "ALTER TABLE announcements AUTO_INCREMENT = 1";
         $subj = "announcements";
     }
 
     if(mysqli_query($conn, $query)){
-        $username = $_SESSION['username'];
-        $adminStampQuery = "INSERT INTO `admin_logs`(`username`, `activity`) VALUES ('$username','Permanently deleted $subj')";
-        mysqli_query($conn, $adminStampQuery);
-        echo 1;
+        if(mysqli_query($conn, $subQuery)){
+            $username = $_SESSION['username'];
+            $adminStampQuery = "INSERT INTO `admin_logs`(`username`, `activity`) VALUES ('$username','Permanently deleted $subj')";
+            mysqli_query($conn, $adminStampQuery);
+            echo 1;
+        }
+        else{
+            echo 0;
+        }
     }
     else{
         echo 0;
